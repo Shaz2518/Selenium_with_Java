152338ee1ccd53b9aa29e9663547d108a8cb4736
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-07-07 22:56:14 +0530

    Star Pattern

--- PatternPrograms_A-Z/src/Alphabet_Square.java
+++ PatternPrograms_A-Z/src/Alphabet_Square.java
@@ -5,13 +5,17 @@ public class Alphabet_Square {
 	{
 		System.out.println("Enter a Character");
 		Scanner scr = new Scanner(System.in);
-		
 		int rows = scr.nextInt();
+		/* A B C D
+		 * A B C D
+		 * A B C D
+		 * A B C D
+		 */
 		for(int i=1; i<=rows; i++)
 		{
-			for(int j=65; j>=65; j++)
+			for(int j=1;j<=rows; j++)
 			{
-				System.out.print(j + " ");
+				System.out.print((char)(j+64) + " ");
 			}
 			System.out.println();
 		}
