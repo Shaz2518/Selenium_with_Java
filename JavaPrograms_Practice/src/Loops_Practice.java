8119a1f264a08afaa3f946e2f9cceafe1da085f9
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-07-08 12:48:06 +0530

    Patterns

---
+++ JavaPrograms_Practice/src/Loops_Practice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Loops_Practice {
+
+	public static void main(String[] args) {
+		for(int i =10; i<=16; i++)
+		{
+			//System.out.println("Hello");
+		}
+		
+		//Print numbers from 1-n
+		/*Scanner scr = new Scanner(System.in);
+		System.out.println("Enter Starting number");
+		int start = scr.nextInt();
+		System.out.println("Enter Ending number");
+		int end = scr.nextInt();
+		for (int i=start; i<=end; i++)
+		{
+			System.out.println(i);
+		}
+		*/
+		for(int i=10; i>=0; i--)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
