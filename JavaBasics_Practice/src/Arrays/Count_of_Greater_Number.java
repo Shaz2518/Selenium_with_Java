ebbca7155e366e7891a0727844d19749d47a84d6
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-07-19 01:11:35 +0530

    Arrays Practice

--- JavaBasics_Practice/src/Arrays/Count_of_Greater_Number.java
+++ JavaBasics_Practice/src/Arrays/Count_of_Greater_Number.java
@@ -6,29 +6,26 @@ public class Count_of_Greater_Number {
 
 	public static void main(String[] args) {
 		Scanner scr = new Scanner(System.in);
-		
-		//Size of an Array
+
+		// Size of an Array
 		System.out.println("Enter the size of an Array: ");
 		int size = scr.nextInt();
 		int[] arr = new int[size];
-		
-		//Take elements on an Array
+
+		// Take elements on an Array
 		System.out.println("Enter elements of an Array: ");
-		for(int i=0; i<size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			arr[i] = scr.nextInt();
 		}
-		
+
 		// Element to be compared
 		System.out.println("Enter an element to be compared");
 		int num = scr.nextInt();
-		
-		//Comparison
-		int count =0;
-		for(int i=0; i<size; i++)
-		{
-			if(arr[i]>num)
-			{
+
+		// Comparison
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (arr[i] > num) {
 				count++;
 			}
 		}
--- JavaBasics_Practice/src/Arrays/Reverse_Array_Swap.java
+++ JavaBasics_Practice/src/Arrays/Reverse_Array_Swap.java
@@ -2,23 +2,20 @@ package Arrays;
 
 public class Reverse_Array_Swap {
 
-	public static void print(int[] arr)
-	{
-		for(int i=0; i<arr.length; i++)
-		{
+	public static void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
-	
+
 	public static void main(String[] args) {
-		int[] arr = { 83, 11, 38, 69, 72, 45, 15, 91};
-		int i = 0, j= arr.length-1;
-		
+		int[] arr = { 83, 11, 38, 69, 72, 45, 15, 91 };
+		int i = 0, j = arr.length - 1;
+
 		System.out.println("Before Reverse: ");
 		print(arr);
-		while(i<=j)
-		{
+		while (i <= j) {
 			int temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
